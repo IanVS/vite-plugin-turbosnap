6d0c575d994a78c6cf7dbf9eb6b6ef042ea86e56
dannytce <dankijkov@gmail.com>
2023-04-23 16:18:07 +0200

    fix: normalize dep ids in StatsMap

--- src/index.ts
+++ src/index.ts
@@ -79,8 +79,8 @@ export default function pluginTurbosnap({
     importers?: readonly string[]
   ): Module {
     return {
-      id: normalize(filename),
-      name: normalize(filename),
+      id: filename,
+      name: filename,
       reasons: createReasons(importers),
     };
   }
@@ -96,7 +96,8 @@ export default function pluginTurbosnap({
         mod.importedIds
           .concat(mod.dynamicallyImportedIds)
           .filter((name) => isUserCode(name))
-          .forEach((depId) => {
+          .forEach((depIdUnsafe) => {
+            const depId = normalize(depIdUnsafe);
             if (statsMap.has(depId)) {
               const m = statsMap.get(depId);
               if (m) {
